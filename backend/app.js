3d9bb73dd8513c39b3b48841491b1672dfed623c
abdelkrim ouaaddi <abdelkrimouaaddi@gmail.com>
2024-08-25 19:13:20 +0900

    update path file of index.html

--- backend/app.js
+++ backend/app.js
@@ -77,7 +77,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 // Test middleware
