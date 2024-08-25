84679d7960591c169085d0aed1f0a0d5730fa47b
abdelkrim ouaaddi <abdelkrimouaaddi@gmail.com>
2024-08-25 21:58:16 +0900

    update delete review functions

--- client/src/services/reviews/HandleMyReviews.js
+++ client/src/services/reviews/HandleMyReviews.js
@@ -14,7 +14,7 @@ export async function getMyReviews({ userId }) {
 export async function deleteMyReview(reviewId) {
   await axios({
     method: "DELETE",
-    url: `http://127.0.0.1:8000/api/v1/reviews/${reviewId}`,
+    url: `${import.meta.env.VITE_BACKEND_BASEURL}/api/v1/reviews/${reviewId}`,
     withCredentials: true, // Ensure cookies are sent and received
   });
   return;
@@ -22,7 +22,7 @@ export async function deleteMyReview(reviewId) {
 export async function updateMyReview({ reviewId, updatedData }) {
   await axios({
     method: "PATCH",
-    url: `http://127.0.0.1:8000/api/v1/reviews/${reviewId}`,
+    url: `${import.meta.env.VITE_BACKEND_BASEURL}/api/v1/reviews/${reviewId}`,
     data: updatedData,
     withCredentials: true, // Ensure cookies are sent and received
   });
